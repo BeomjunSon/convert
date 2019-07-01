28d51bc5c905eba4ee69885d2e2fb2fd6d741d53
BeomjunSon <qjaens2321@gmail.com>
2019-07-01 14:30:26 +0900

    add weight readme

---
+++ Weight.md
@@ -0,0 +1,5 @@
+# Length convert
+무게 단위를 변환 합니다.
+
+## contributor
+- Geun, Grain, Gram, Kilogram, Kiloton, Milligram, Ounce, Pound, Ton : 손범준(qjaens2321@gmail.com)
--- test/kilogram.php
+++ test/kilogram.php
@@ -2,20 +2,20 @@
 
 require "../../../autoload.php";
 
-use Jiny\Convert\Length\Foot as LengthFoot;
+use Jiny\Convert\Weight\Kilogram as WeightKilogram;
 
 // $obj = new Jiny\Convert\Length\Foot(1);
-$obj = new LengthFoot(1);
+$obj = new WeightKilogram(1);
 
 // 타입확인
 echo $obj::TYPE."\n";
 echo $obj->type()."\n";
 
-// 센치미터 변환
-echo $obj->centi()."\n";
+// 그램 변환
+echo $obj->gram()."\n";
 
 // 직접 지정
-echo $obj->centi(2)."\n";
+echo $obj->gram(2)."\n";
 
 // 헬퍼함수 처리
 // echo convFootCenti(3);
